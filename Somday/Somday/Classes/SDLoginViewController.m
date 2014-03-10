37d167f49ab2baa5292246047541960b66c37758
Freddy So <freddyso0602@gmail.com>
2014-03-10 17:02:39 +0800

    Add login screen

---
+++ Somday/Somday/Classes/SDLoginViewController.m
@@ -0,0 +1,17 @@
+//
+//  SDLoginViewController.m
+//  Somday
+//
+//  Created by Freddy So on 3/10/14.
+//  Copyright (c) 2014 Freddy So. All rights reserved.
+//
+
+#import "SDLoginViewController.h"
+
+@interface SDLoginViewController ()
+
+@end
+
+@implementation SDLoginViewController
+
+@end
