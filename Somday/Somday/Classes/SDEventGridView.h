a37086e629ffb94f829102beec8bc0a0732c798f
steventmk <steventmk@gmail.com>
2014-04-03 17:24:42 +0800

    event grid view

---
+++ Somday/Somday/Classes/SDEventGridView.h
@@ -0,0 +1,13 @@
+//
+//  SDEventGridView.h
+//  Somday
+//
+//  Created by Tao, Steven on 4/3/14.
+//  Copyright (c) 2014 Freddy So. All rights reserved.
+//
+
+#import "SDBaseGridView.h"
+
+@interface SDEventGridView : SDBaseGridView
+
+@end
