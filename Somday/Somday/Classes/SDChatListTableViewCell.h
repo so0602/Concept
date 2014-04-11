db70adf37f0b90c7cadf0889269d214defc3e828
steventmk <steventmk@gmail.com>
2014-04-11 16:24:22 +0800

    no message

---
+++ Somday/Somday/Classes/SDChatListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SDChatListTableViewCell.h
+//  Somday
+//
+//  Created by Tao, Steven on 4/11/14.
+//  Copyright (c) 2014 Freddy So. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDChatListTableViewCell : UITableViewCell
+
+@end
---
+++ Somday/Somday/Classes/SDRecentChatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDRecentChatsViewController.h
+//  Somday
+//
+//  Created by Tao, Steven on 4/11/14.
+//  Copyright (c) 2014 Freddy So. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDRecentChatsViewController : UIViewController
+
+@end
